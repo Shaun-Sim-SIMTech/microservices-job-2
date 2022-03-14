9a12b1d20b1715f1cf368ef570a48d0b9520f306
Shaun-Sim-SIMTech <shaun_sim@simtech.a-star.edu.sg>
2022-03-14 10:59:40 +0800

    Initial commit

---
+++ Model/Booking.cs
@@ -0,0 +1,21 @@
+﻿namespace Taxi_Booking.Model
+{
+    public class Booking
+    {
+        public int Id { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public DateTime Time { get; set; }
+
+        public string? PickupPoint { get; set; }
+
+        public string? Destination { get; set; }
+
+        public double Current_Location_Latitude { get; set; }
+
+        public double Current_Location_Longitude { get; set; }
+
+
+    }
+}
